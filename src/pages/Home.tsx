9a067bcc910093359daf9e492d65262bcecc0587
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-07-17 22:40:27 -0300

    Home page first layout

---
+++ src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIcon from '../assets/images/google-icon.svg'
+
+export function Home() {
+  return (
+    <div>
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div>
+          <img src={logoImg} alt="Letmesk" />
+          <button>
+            <img src={googleIcon} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div>ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <button type="submit">
+              Entrar na sala
+            </button>
+          </form>
+        </div>
+      </main>
+    </div>
+
+  )
+}
